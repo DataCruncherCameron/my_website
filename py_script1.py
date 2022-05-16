926b90e546aea6c0d3edb870fb7d9b96573a1362
Aaron DevOps <mclfarm2@gmail.com>
2022-05-16 17:28:09 +1000

    removed secrets manager and hardcoded in secrets to access the azure db
    Should be able to deploy to Azure and make calls to the azure database.

--- py_script1.py
+++ py_script1.py
@@ -2,96 +2,16 @@ from pymysql import connections
 from flask import Flask, render_template, request, url_for, flash, redirect 
 from werkzeug.exceptions import abort
 
-# Packages for call to secrets manager. 
-import boto3
-import base64
-from botocore.exceptions import ClientError
-
-# Make call to secrets manager 
-def get_secret():
-
-    secret_name = "blog_website/mysql"
-    region_name = "ap-southeast-1"
-
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    # We rethrow the exception by default.
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-    else:
-        # Decrypts secret using the associated KMS key.
-        # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if 'SecretString' in get_secret_value_response:
-            secret = get_secret_value_response['SecretString']
-        else:
-            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
-
-    secret=secret[1:-2]
-    secret=secret.replace('"','')
-    secret_chunk_array=secret.split(',')
-
-    keys=[]
-    values=[]
-
-    for i in range(0,len(secret_chunk_array)):
-            key_value=secret_chunk_array[i].split(':')
-            keys.append(key_value[0])
-            values.append(key_value[1])
-
-    secret_dict=dict(zip(keys,values))
-    return(secret_dict)
-
-secret=get_secret()
-            
-
-
-
-
-
-
-
-
 
 
 headers=('id','created','title','content')
 
 db_conn = connections.Connection(
-    host = secret['host'],
-    port = secret['port'],
-    user= secret['user'],
-    password= secret['password'],
-    db= secret['dbInstanceIdentifier']
+    host = 'blog-mysql-database.mysql.database.azure.com',
+    port = 3306,
+    user= 'cam',
+    password= 'fearnothing1918B',
+    db= 'db'
 )
 
 
